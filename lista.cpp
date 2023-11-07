c04ebe45858fe03ef8274b44c0ae5074f1db5816
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2023-11-07 11:57:21 -0300

    update lista

--- lista.cpp
+++ lista.cpp
@@ -111,6 +111,84 @@ void Lista<T>::inserirPosicao(int posicao, T entrada){
     }
 }
 
+template<class T>
 QString Lista<T>::obterDados()const{
     
+}
+
+template<class T>
+T Lista<T>::retirarInicio(){
+    if (estaVazia()){
+        throw QString("Lista ja vazia, nao foi possivel retirar");
+    }
+    NO<T>* aux = inicio;
+    if (quantidadeElementos == 1){
+        quantidadeElementos = 0;
+        inicio = fim = 0;
+        T valor = aux->getDado();
+        delete aux;
+        return valor;
+    }
+    T valor = inicio->getDado();
+    inicio = inicio->getProximo();
+    inicio->setAnterior(0);
+    delete aux;
+    --quantidadeElementos;
+    return valor;
+}
+
+template<class T>
+T Lista<T>::retirarFim(){
+    if (estaVazia()){
+        throw QString("Lista ja vazia, nao foi possivel retirar");
+    }
+    if (quantidadeElementos == 1){
+        retirarInicio();
+        return;
+    }
+    NO<T>* aux = fim;
+    T valor = fim->getDado();
+    fim = fim->getAnterior();
+    fim->setProximo(0);
+    delete aux;
+    --quantidadeElementos;
+    return valor;
+}
+
+template<class T>
+T Lista<T>::retirarPosicao(int posicao){
+    if (estaVazia()){
+        throw QString("Lista ja vazia, nao foi possivel retirar");
+    }
+    if (posicao < 0 || posicao >= quantidadeElementos){
+        throw QString("Posicao invalida, nao pode ser menor que 0 ou maior igual que a quantidade de elementos");
+    }
+    if (quantidadeElementos == 1 || posicao == 0){
+        retirarInicio();
+        return;
+    }
+    if (posicao == quantidadeElementos - 1){
+        retirarFim();
+        return;
+    }
+    NO<T>* aux;
+    if (posicao < quantidadeElementos / 2){
+        aux = inicio;
+        for (int i = 0; i < posicao; ++i){
+            aux = aux->getProximo();
+        }
+    }else{
+        aux = fim;
+        for (int i = 0; i > posicao; --i){
+            aux = aux->getAnterior();
+        }
+    }
+    NO<T>* anterior = aux->getAnterior();
+    NO<T>* proximo = aux->getProximo();
+    anterior->setProximo(proximo)
+    proximo.setAnterior(anterior);
+    T valor = aux->getDado();
+    delete aux;
+    --quantidadeElementos;
+    return valor;
 }
